1f3265341f32854192ce9f05db82ddd0a5d83d6f
alam <20175131@itla.edu.do>
2019-12-01 04:11:48 -0400

    EL apartado de mantenimientos esta semi-realizado, las funciones de crear y editar fueron diseñadas y implementas, aun faltan arreglos estiticos pero trae consigo todas la funcionalidades requeridas.
    OJO: El metodo de desarrollo fue database first con entity framework.

---
+++ Proyecto/Controllers/ControllersMantenimiento/DepartamentoesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto.Models;
+
+namespace Proyecto.Controllers.ControllersMantenimiento
+{
+    public class DepartamentoesController : Controller
+    {
+        private RHumanosDBEntities db = new RHumanosDBEntities();
+
+        // GET: Departamentoes
+        public ActionResult Index()
+        {
+            return View(db.Departamento.ToList());
+        }
+
+        // GET: Departamentoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Departamento departamento = db.Departamento.Find(id);
+            if (departamento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(departamento);
+        }
+
+        // GET: Departamentoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Departamentoes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Codigo_departamento,Nombre")] Departamento departamento)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Departamento.Add(departamento);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(departamento);
+        }
+
+        // GET: Departamentoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Departamento departamento = db.Departamento.Find(id);
+            if (departamento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(departamento);
+        }
+
+        // POST: Departamentoes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Codigo_departamento,Nombre")] Departamento departamento)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(departamento).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(departamento);
+        }
+
+        // GET: Departamentoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Departamento departamento = db.Departamento.Find(id);
+            if (departamento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(departamento);
+        }
+
+        // POST: Departamentoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Departamento departamento = db.Departamento.Find(id);
+            db.Departamento.Remove(departamento);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
